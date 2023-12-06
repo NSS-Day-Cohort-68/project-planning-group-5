5f2054f4335b32bb907b2931fd25bee094ef1183
Zachary Thacker <zthacker@outlook.com>
2023-12-06 13:49:04 -0600

    added new ID functionality to add item

--- items.js
+++ items.js
@@ -44,12 +44,26 @@ let userItemName = readline.question("What is the name of the item?: ")
 let userQuantity = readline.question("How many are there?: ")
 //create a new item based off the users input
 const userItem = { 
-  id: 6,
+  id: createItemId(),
   name: userItemName,
   quantity: userQuantity,
 }
 //add the item to our inventory  
 items.push(userItem)
+}
+//create a new id for a new item
+const createItemId = () => {
+  //we have an array with ids, we need to make a new id that is higher than the highest current id
+   //collect the ids
+   const ids = []
+  for (const item of items) {
+      ids.push(item.id)
+   }
+   //find the biggest id
+  const biggestId = Math.max(...ids)
+   //return ++
+   const newId = biggestId + 1
+   return newId
 }
 
 // edit an item's details
